4661ee20e5b853f13ed952bcc4c9657350d17c6c
Nayeef sarker Nafi <155153560+MeNafi@users.noreply.github.com>
2026-06-12 21:46:28 +0600

    Update the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import Router from './Router/Router'; // Adjust path if needed
 
+
 function App() {
   return (
     <RouterProvider router={Router} />
