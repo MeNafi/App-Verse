e4a7b4a2ca4a33c6205183c2493ce0db63f06f22
Nayeef sarker Nafi <155153560+MeNafi@users.noreply.github.com>
2026-06-15 21:35:09 +0600

    Update the code

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import Router from "./Router/Router";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={Router} />
