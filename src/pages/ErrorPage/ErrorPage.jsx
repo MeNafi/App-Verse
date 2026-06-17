a6d7d423a280f480b907bf3b3eff95c40fb63b37
Nayeef sarker Nafi <155153560+MeNafi@users.noreply.github.com>
2026-06-17 22:21:34 +0600

    Update the code

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+    
     // Get error details from the router
     const error = useRouteError();
 
